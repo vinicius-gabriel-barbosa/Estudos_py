80ce4c098d755b07cfabf8777e2bd912ebbbddd8
Vinicius Gabriel <vinicius.gabriel.barbosa.lara@gmail.com>
2026-04-19 10:54:09 -0300

    algumas alteracoes pequenas

---
+++ calculadora_imc/calculadora_imc1.py
@@ -0,0 +1,18 @@
+"""Calculadora de imc #1"""
+
+def obter_dados_usuarios():
+    """Essa def coleta os dados da pessoa que vai usar como peso e altura para calcular o imc"""
+    peso = float(input("Qual o seu peso: "))
+    altura = float(input("Qual a sua altura, em metros: "))
+    return peso, altura #aqui vamos retornar o peso e a altura para fora da def
+
+def main():
+    """vamos pegar o peso e a altura do obter_dados_usuarios e vamos calcular o imc"""
+    peso, altura = obter_dados_usuarios()
+    imc = peso / (altura ** 2) #aqui vamos calcular os dados e falar que o imc e o peso/altura²
+    print(f"Seu IMC é: {imc:.2f}") #vamos colocar na tela o imc e apenas duas casas depois da virgula
+
+if __name__ == '__main__':
+    main()
+
+
